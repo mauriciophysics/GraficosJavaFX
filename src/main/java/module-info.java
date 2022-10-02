ddc9ca8dac26d0fc5c39c89a9bd7d24e776b1e9e
Maurício Borges <40478758+mauriciophysics@users.noreply.github.com>
2022-10-02 03:19:40 -0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module br.com.mauricioborges.graficos {
-    requires javafx.controls;
-    requires javafx.fxml;
-     
-    opens br.com.mauricioborges.graficos.gui to javafx.fxml;
-  
-    exports br.com.mauricioborges.graficos.gui;
-    exports br.com.mauricioborges.graficos.math;
-}
